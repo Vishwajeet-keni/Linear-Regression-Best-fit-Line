a35690828a402f2c3ce485d57926711894059fc0
Vishwajeet-keni <kenivishwajeet874@gmail.com>
2025-09-07 14:36:31 +0530

    optimised

--- linearFit.py
+++ linearFit.py
@@ -7,42 +7,42 @@ x=zeros(n,float)     # array to store x
 xSq=zeros(n,float)   # array to store x square
 y=zeros(n,float)     # array to store y
 xy=zeros(n,float)    # array to store x.y
+
+
 i=0
 while i<n:                                # loop to collect x co-ordinat of Data points and store in array (x)
     x[i]=float(input(f"Enter x{i+1}: "))  
     i+=1
+Sum_X=sum(x)     # Sums all the valus stored in array (x)
 
 i=0
 while i<n:                                # loop to collect y co-ordinat of Data points and store in array (y)
     y[i]=float(input(f"Enter y{i+1}: "))
     i+=1
-Sum_X=sum(x)     # Sums all the valus stored in array (x)
-print("Sum_X: ",Sum_X)    
-
 Sum_Y=sum(y)     # Sums all the valus stored in array (y)
-print("Sum_X: ",Sum_Y)
-
 
 i=0
 while i<n:             # loop which square the values stored in x and store in (xSq)
     xSq[i]=x[i]**2
     i+=1
 Sum_Xsq=sum(xSq)     # Sums all the valus stored in array (xSq)
-print("Sum_Xsq: ",Sum_Xsq)      
-
+      
 i=0
 while i<n:            # loop which multiplies the value stored in x & y then save them in array(xy)
     xy[i]=x[i]*y[i]
     i+=1
 Sum_XY=sum(xy)     # Sums all the valus stored in array (xy)
-print("Sum_XY: ",Sum_XY)
+
 
 m=(n*Sum_XY - Sum_X*Sum_Y)/(n*Sum_Xsq - (Sum_X)**2)     # Calculating Slope
+c=(Sum_Y - (m*Sum_X))/n     #Calculating Intercept
+
+print("Sum_X: ",Sum_X)
+print("Sum_Y: ",Sum_Y)
+print("Sum_Xsq: ",Sum_Xsq)
+print("Sum_XY: ",Sum_XY) 
 print("Slope m= ", m)
-
-c=(Sum_Y - (m*Sum_X))/n     #Calculating Intercept 
 print("Intercept c= ", c)
-
 print("Final equation of the Best fit line is:")
 if c>=0:
     print(f"y={m}x+{c}")
