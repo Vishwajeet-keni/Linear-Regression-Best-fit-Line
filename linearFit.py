1bcd54194001d7bb6d2a50751416c4b425b9e6be
Vishwajeet-keni <kenivishwajeet874@gmail.com>
2025-10-01 20:41:08 +0530

    optimixation 2

--- linearFit.py
+++ linearFit.py
@@ -11,7 +11,7 @@ xy=[]    # array to store x.y
 
 i=0
 while i<n:                                # loop to collect x co-ordinat of Data points and store in array (x)
-    x.append(float(input(f"Enter x{i+1}: ")))
+    x.append(float(input(f"\nEnter x{i+1}: ")))
     y.append(float(input(f"Enter y{i+1}: ")))
     i+=1 
 Sum_X=sum(x)     # Sums all the valus stored in array (x)
@@ -24,16 +24,16 @@ Sum_XY=sum([x[i]*y[i] for i in range(0,n)])
 m=(n*Sum_XY - Sum_X*Sum_Y)/(n*Sum_Xsq - (Sum_X)**2)     # Calculating Slope
 c=(Sum_Y - (m*Sum_X))/n     #Calculating Intercept
 
-print(f'''Sum_X: {Sum_X}
-          Sum_XY: {Sum_XY}
-          Slope m= {m}
-          Intercept c= {c}''')
-print("Final equation of the Best fit line is:")
+print(f'''\nSum_X: {Sum_X}
+Sum_XY: {Sum_XY}
+Slope m= {m}
+Intercept c= {c}''')
+print("\nFinal equation of the Best fit line is:")
 if c>=0:
     print(f"y={m}x+{c}")
 else:
     print(f"y={m}x3{c}")
-
+    
 plt.scatter(x,y,color='blue',marker="o",label="Data Points")
 x=np.linspace(min(x),max(x),1000)
 y_pred=m*x+c
