bc2ef25521a88d898aa3516ba6d7c60184690d13
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2025-11-19 18:48:27 +0530

    commit from linux(check), and code simplification

--- linearFit.py
+++ linearFit.py
@@ -4,35 +4,35 @@ import numpy as np
 n=int(input("Enter numbers if Data Points: "))
 
 x=[]   # array to store x
-xSq=[]   # array to store x square
 y=[]     # array to store y
-xy=[]    # array to store x.y
-
 
 i=0
 while i<n:                                # loop to collect x co-ordinat of Data points and store in array (x)
     x.append(float(input(f"\nEnter x{i+1}: ")))
     y.append(float(input(f"Enter y{i+1}: ")))
-    i+=1 
-Sum_X=sum(x)     # Sums all the valus stored in array (x)
-Sum_Y=sum(y)     # Sums all the valus stored in array (y)
+    i+=1
 
-xSq=[x**2 for x in x]
-Sum_Xsq=sum(xSq)
-Sum_XY=sum([x[i]*y[i] for i in range(0,n)])
+x=np.array(x)
+y=np.array(y)
+
+sumX=sum(x)     # Sums all the valus stored in array (x)
+sumY=sum(y)     # Sums all the valus stored in array (y)
+
+sumXsq=sum(x**2)
+sumXY=sum(x*y)
       
-m=(n*Sum_XY - Sum_X*Sum_Y)/(n*Sum_Xsq - (Sum_X)**2)     # Calculating Slope
-c=(Sum_Y - (m*Sum_X))/n     #Calculating Intercept
+m=(n*sumXY - sumX*sumY)/(n*sumXsq - (sumX)**2)     # Calculating Slope
+c=(sumY - (m*sumX))/n     #Calculating Intercept
 
-print(f'''\nSum_X: {Sum_X}
-Sum_XY: {Sum_XY}
+print(f'''\nsumX: {sumX}
+sumXY: {sumXY}
 Slope m= {m}
 Intercept c= {c}''')
 print("\nFinal equation of the Best fit line is:")
 if c>=0:
     print(f"y={m}x+{c}")
 else:
-    print(f"y={m}x3{c}")
+    print(f"y={m}x{c}")
     
 plt.scatter(x,y,color='blue',marker="o",label="Data Points")
 x=np.linspace(min(x),max(x),1000)
